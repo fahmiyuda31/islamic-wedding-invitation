17479142410b558dc92a2fc2d3eea58d8427f82d
ravelwareFahmi <60086744+fahmiyuda31@users.noreply.github.com>
2025-03-29 17:34:25 +0700

    Update ScanQR.jsx

--- src/pages/ScanQR.jsx
+++ src/pages/ScanQR.jsx
@@ -33,7 +33,7 @@ const ScanQR = ({ db }) => {
     const handleScan = (result) => {
         // console.log('Scan result:', result);
         const rawResult = result[0]?.rawValue;
-        alert(rawResult)
+        // alert(rawResult)
         if (rawResult) {
             findGuest(rawResult)
         }
