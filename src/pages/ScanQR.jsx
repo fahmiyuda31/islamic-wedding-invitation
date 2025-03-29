848d21a4d43ea9980a53fbd7d4543ded35b03cf3
ravelwareFahmi <60086744+fahmiyuda31@users.noreply.github.com>
2025-03-29 17:34:17 +0700

    Update ScanQR.jsx

--- src/pages/ScanQR.jsx
+++ src/pages/ScanQR.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
 import { Modal } from 'antd';
+import { collection, doc, getDocs, serverTimestamp, updateDoc } from 'firebase/firestore';
 
 const ScanQR = ({ db }) => {
 
+
     const updateGuest = (guestName) => {
-        const guestRef = db.collection('guests').doc(guestName);
-        const updatedAt = serverTimestamp();
-        guestRef.update({ check_in: updatedAt }).then(() => {
-            // console.log('Guest updated successfully!');
-            Modal.success({ content: `Selamat Datang ${guestName}, Silahkan Ambil Souvenir nya` })
+        const colRef = collection(db, 'guest');
+        const docRef = doc(colRef, guestName);
+        const check_in = serverTimestamp();
+        updateDoc(docRef, { check_in }).then(() => {
+            console.log('Document updated successfully!');
         }).catch(error => {
-            // console.error('Error updating guest: ', error);
+            console.error('Error updating document: ', error);
             Modal.error({ content: 'Error updating guest' })
         });
     }
     const findGuest = (guestName) => {
-        const guestRef = db.collection('guests').doc(guestName);
-        guestRef.get().then((doc) => {
+        const colRef = collection(db, 'guest');
+        const docRef = doc(colRef, guestName);
+        getDocs(docRef).then().then((doc) => {
             if (doc.exists) {
                 // console.log('Guest found:', doc.data());
                 updateGuest(guestName);
@@ -41,15 +43,15 @@ const ScanQR = ({ db }) => {
             display: 'flex',
             justifyContent: 'center',
             flexDirection: 'column',
-            alignItems: 'center', 
+            alignItems: 'center',
         }}>
-            
+
             <p className='p-4 text-center font-bold text-2xl'>Silahkan Scan QR Code Tamu</p>
             <Scanner
                 onScan={(result) => handleScan(result)}
                 facingMode="user" // Show the front camera
                 resolution={1280} // Set the resolution to 1280x720
-                style={{ width: '100%', height: '100%',  }} // Set the camera view to full screen
+                style={{ width: '100%', height: '100%', }} // Set the camera view to full screen
             />
         </div>
     )
